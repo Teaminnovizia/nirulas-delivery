aa8d0a5a238713828911bac4d76b8ada7b6945f2
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2023-09-16 16:09:21 +0530

    Update: Made carousal loadable

--- components/Pages/Home/HeroBanner.tsx
+++ components/Pages/Home/HeroBanner.tsx
@@ -1,20 +1,60 @@
+'use client';
+
 import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
+const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    nextArrow: false,
+    prevArrow: false
+};
 
 const HeroBanner = () => {
     return (
         <section className='w-full'>
-            <div className='w-full relative aspect-[16/5]'>
-                <Image
-                    src='/Images/home/home_slide_bg1.jpeg'
-                    alt='Banner'
-                    quality={100}
-                    fill
-                    priority
-                    className='object-cover'
-                />
-            </div>
+            <Slider {...settings} className='w-full'>
+                {DATA?.map((x) => (
+                    <div key={x?.alt} className='w-full relative aspect-[16/5]'>
+                        <Image
+                            src={x?.image}
+                            alt={x?.alt}
+                            quality={100}
+                            fill
+                            priority
+                            className='object-cover'
+                        />
+                    </div>
+                ))}
+            </Slider>
         </section>
     )
 }
 
 export default HeroBanner;
+
+const DATA = [
+    {
+        image: '/Images/home/home_slide_bg1.png',
+        alt: 'Jamun'
+    },
+    {
+        image: '/Images/home/home_slide_bg2.png',
+        alt: 'Pizza'
+    },
+    {
+        image: '/Images/home/home_slide_bg3.png',
+        alt: 'Burger'
+    },
+    {
+        image: '/Images/home/home_slide_bg4.png',
+        alt: 'Healthy'
+    },
+]
