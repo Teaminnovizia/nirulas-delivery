8f64542f79f6783eb8d9fb2bda524835f9156dd2
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2023-09-16 17:11:44 +0530

    Fix: types for react slick...

---
+++ components/Pages/Home/Menu.tsx
@@ -0,0 +1,11 @@
+const Menu = () => {
+    return (
+        <section className='w-full sm:px-8 px-3'>
+            <div className='flex flex-col items-center gap-10 max-w-6xl w-full mx-auto sm:py-8 py-4'>
+                
+            </div>
+        </section>
+    )
+}
+
+export default Menu;
