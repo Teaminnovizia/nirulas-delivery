d9ca5314597b87d591d29f16918a1d74bfe8d411
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2023-09-24 00:37:23 +0530

    Update: used ref instead of id for popup container

--- components/Common/PopUps/PopupContainer.tsx
+++ components/Common/PopUps/PopupContainer.tsx
@@ -2,21 +2,24 @@
 
 import { PopupContainerProps } from "@/types/PopUpsTypes";
 import gsap from "gsap";
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 
 const PopupContainer = ({ children, ...props }: PopupContainerProps) => {
+    const popup_container_ref = useRef(null);
+    const popup_inner_ref = useRef(null);
+
     useLayoutEffect(() => {
         const tl = gsap.timeline();
 
-        tl.to('#popup_container', { duration: 1, opacity: 100, ease: "sine.out" }, 'a')
-            .to('#popup_inner', { duration: 1.5, y: 0, opacity: 100, ease: 'elastic.out(1, 0.5)' }, 'a')
+        tl.to(popup_container_ref?.current, { duration: 1, opacity: 100, ease: "sine.out" }, 'a')
+            .to(popup_inner_ref?.current, { duration: 1.5, y: 0, opacity: 100, ease: 'elastic.out(1, 0.5)' }, 'a')
     }, [])
 
     const setClose = (onAnimationComplete?: () => void) => {
         const tl = gsap.timeline();
 
-        tl.to('#popup_container', { duration: 0.6, opacity: 0 }, 'b')
-            .to('#popup_inner', {
+        tl.to(popup_container_ref?.current, { duration: 0.6, opacity: 0 }, 'b')
+            .to(popup_inner_ref?.current, {
                 duration: 0.6, y: '100vh', opacity: 0, onComplete: () => {
                     props.setShowPopup(false);
                     if (onAnimationComplete) onAnimationComplete();
@@ -25,8 +28,8 @@ const PopupContainer = ({ children, ...props }: PopupContainerProps) => {
     }
 
     return (
-        <div onClick={() => setClose()} id='popup_container' className='fixed h-screen w-screen left-0 top-0 flex items-center justify-center bg-gray-200/50 z-[999] opacity-0 px-3 py-4'>
-            <div onClick={(e) => e?.stopPropagation()} id='popup_inner' style={{ maxWidth: props.maxWidth }} className={`w-full mx-auto flex items-center justify-center translate-y-[100vh] opacity-0`}>
+        <div onClick={() => setClose()} ref={popup_container_ref} className='fixed h-screen w-screen left-0 top-0 flex items-center justify-center bg-gray-200/50 z-[999] opacity-0 px-3 py-4'>
+            <div onClick={(e) => e?.stopPropagation()} ref={popup_inner_ref} style={{ maxWidth: props.maxWidth }} className={`w-full mx-auto flex items-center justify-center translate-y-[100vh] opacity-0`}>
                 {children(setClose)}
             </div>
         </div>
